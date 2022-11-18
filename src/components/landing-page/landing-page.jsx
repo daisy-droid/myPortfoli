80309cdb57d46765e7bc1c86e5cda266fed40e00
PU MAC2 <pumac2@PUMAC2.local>
2022-11-18 14:13:48 +0200

    first commit

---
+++ src/components/landing-page/landing-page.jsx
@@ -0,0 +1,34 @@
+import "../landing-page/landing-page.css"
+
+
+const Landing = ()=>
+{
+    return ( <div className="i">
+    <div className="i-left">
+        <div className="i-wrapper">
+            <h2 className="i-intro">Hi, My name is </h2>
+            <h1 className="i-name"> Ipeleng Mothibi</h1>
+            <div className="i-title">
+                <div className="i-title-wrapper">
+                    <div className="i-title-item">Web Developer</div>
+                    <div className="i-title-item">UI/UX Designer</div>
+                    <div className="i-title-item">Web Developer</div>
+                    <div className="i-title-item">Web Developer</div>
+                    <div className="i-title-item">Web Developer</div>
+                </div>
+            </div>
+            <div className="i-desc">
+            I am full-stack developer who enjoys trying out new technology, I pride myself in being a hard-worker that is not intimidated by learning new technologies which will be beneficial when the company has to adapt to new technologies and or uses technologies I'm not familiar with
+            </div>
+        
+        </div>
+    
+    </div>
+    <div className="i-right">
+        <div className="i-bg"></div>
+        <div className="i-bg2"></div>
+    </div>
+       
+</div>)
+}
+export default Landing
